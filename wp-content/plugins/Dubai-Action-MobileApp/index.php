60b3254328a0e849b21e8831f1d1b3203fc4b48c
Deen Dabbagh <82648982+Deen-Dabbagh@users.noreply.github.com>
2022-01-24 14:42:26 +0400

    changed photosize

--- wp-content/plugins/Dubai-Action-MobileApp/index.php
+++ wp-content/plugins/Dubai-Action-MobileApp/index.php
@@ -96,7 +96,7 @@ register_rest_route( 'da/v2', 'attractions/', array(
 
 function post_featured_image_json( $data, $post, $context ) {
   $featured_image_id = $data->data['featured_media']; // get featured image id
-  $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'original' ); // get url of the original size
+  $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'medium' ); // get url of the original size
 
   if( $featured_image_url ) {
     $data->data['featured_image_url'] = $featured_image_url[0];
