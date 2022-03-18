9701c4140c9f0508b61944c334fc71c6e607597b
Deen Dabbagh <82648982+Deen-Dabbagh@users.noreply.github.com>
2022-03-18 11:17:41 +0400

    check tags

--- wp-content/plugins/Dubai-Action-MobileApp/index.php
+++ wp-content/plugins/Dubai-Action-MobileApp/index.php
@@ -88,9 +88,10 @@ function da_gem($data) {
 
 function da_gems($data) {
     // echo $_GET['tags'];
-    $tags=explode(",",$_GET['tags']);
+   
 
     if ($tags){
+        $tags=explode(",",$_GET['tags']);
 	$args = [
 		'numberposts' => 99999,
 		'post_type' => 'hidden',
