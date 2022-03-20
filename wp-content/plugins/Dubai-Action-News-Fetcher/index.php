16fdf319c9a296887458201d283f5f7016a3c2fa
Deen Dabbagh <82648982+Deen-Dabbagh@users.noreply.github.com>
2022-03-20 17:24:45 +0400

    updated news source

--- wp-content/plugins/Dubai-Action-News-Fetcher/index.php
+++ wp-content/plugins/Dubai-Action-News-Fetcher/index.php
@@ -42,28 +42,17 @@ if ($err) {
 	echo "cURL Error #:" . $err;
 } else {
     $articles=json_decode($response,true);
-	// print_r ($arr['articles'][0]['title']);
-    // var_dump($arr['articles'][0]['title']);
 }
 $i=0;
-// echo $articles[1]['title'];
-// echo $articles[2]['title'];
-// echo $articles[3]['title'];
 
-//  print_r($articles['articles'][0]['title']);
-//echo $articles[1]['title'];
-// print_r($articles);
  foreach ($articles['articles'] as $news){
-    //  $news=$articles['articles'][0];
-    //  print_r( $news);
-//  echo $news['title'];
-    // print_r("News title:"+$news[$i]['title']);
+  
     $page = get_page_by_title($news['title'], OBJECT, 'post');
-    // print_r($page);
+
     if (is_null($page)){
     $my_post = array(
         'post_title'    => wp_strip_all_tags( $news['title'] ),
-        'post_content'  =>  $news['summary'],
+        'post_content'  =>  $news['summary']." <br/><br/><a href='"+$news['link']+"Source</a>",
         'post_status'   => 'publish',
         'post_author'   => 1,
         'post_category' => array( 1 )
@@ -74,27 +63,7 @@ $i=0;
 
       update_post_meta($post_id,'featuredimg', $news['media']);
 
-    //   $image = $news['media'];
-
-    //   $attachment_file_type = wp_check_filetype(basename($image), null);
-      
-    //   $wp_upload_dir = wp_upload_dir();
-      
-    //   $attachment_args = array(
-    //       'guid'           => $wp_upload_dir['url'] . '/' . basename($image),
-    //       'post_title'     => preg_replace('/\.[^.]+$/', '', basename($image)),
-    //       'post_mime_type' => $attachment_file_type['type']
-    //   );
-      
-    //   $attachment_id = wp_insert_attachment($attachment_args, $image, $post_id);
-      
-    //   require_once(ABSPATH . 'wp-admin/includes/image.php');
-      
-    //   $attachment_meta_data = wp_generate_attachment_metadata($attachment_id, $image);
-      
-    //   wp_update_attachment_metadata($attachment_id, $attachment_meta_data);
-      
-    //   set_post_thumbnail($post_id, $attachment_id);
+    
     }
 }
 }
