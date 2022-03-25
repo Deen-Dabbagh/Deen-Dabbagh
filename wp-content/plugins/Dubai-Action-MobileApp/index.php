0a15f2758c9411abd0f0ff8bd3801a08b6238074
Deen Dabbagh <82648982+Deen-Dabbagh@users.noreply.github.com>
2022-03-25 14:18:10 +0400

    removed counter

--- wp-content/plugins/Dubai-Action-MobileApp/index.php
+++ wp-content/plugins/Dubai-Action-MobileApp/index.php
@@ -69,9 +69,9 @@ $args = [
 $i=0;
     $posts = get_posts($args);
     // foreach($posts as $post) {
-    $data[$i]['modified'] = $posts[$i]->post_modified;
-    $data[$i]['count'] = count($posts);
-    $data[$i]['post_title'] = $posts[$i]->post_title;
+    $data['modified'] = $posts[$i]->post_modified;
+    $data['count'] = count($posts);
+    $data['post_title'] = $posts[$i]->post_title;
     // $i++;
     // }
     return $data;
