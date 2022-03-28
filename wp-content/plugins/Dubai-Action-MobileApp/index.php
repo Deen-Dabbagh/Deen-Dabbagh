feb262c29a0d1960f02d3fd88d353bbd65a0c3b0
Deen Dabbagh <82648982+Deen-Dabbagh@users.noreply.github.com>
2022-03-28 10:29:49 +0400

    removed contributor code

--- wp-content/plugins/Dubai-Action-MobileApp/index.php
+++ wp-content/plugins/Dubai-Action-MobileApp/index.php
@@ -12,13 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-//Allow Contributors to Upload Media
-if ( current_user_can('contributor') && !current_user_can('upload_files') )
-add_action('admin_init', 'allow_contributor_uploads');
-function allow_contributor_uploads() {
-     $contributor = get_role('contributor');
-     $contributor->add_cap('upload_files');
-}
+
 
 // function da_rest_prepare_post($data, $post, $request) {
 //     // $_data = $data->data;
